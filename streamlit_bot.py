a275753411732bfadcd80133bd274271af2ad0a3
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-02-06 18:08:11 -0300

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -1,23 +1,13 @@
 import streamlit as st
-import spacy
 
 st.title("Chatbot")
-
-nlp = spacy.load("en_core_web_sm")
 
 user_input = st.text_input("User:", value="")
 
 if user_input:
-    doc = nlp(user_input)
-    intent = None
-    for token in doc:
-        if token.text.lower() in ["hi", "hello", "hey"]:
-            intent = "greeting"
-        elif token.text.lower() in ["bye", "goodbye"]:
-            intent = "farewell"
-    if intent == "greeting":
+    if "hi" in user_input.lower():
         st.write("Bot: Hello! How can I help you today?")
-    elif intent == "farewell":
+    elif "bye" in user_input.lower():
         st.write("Bot: Goodbye! Have a nice day.")
     else:
         st.write("Bot: Sorry, I don't understand. Can you try asking me something else?")
