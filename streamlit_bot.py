e5bd01d148133a1c46c2e1f1998971081cdc9e2a
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-02-08 14:01:06 -0300

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -5,11 +5,15 @@ st.title("Chatbot")
 user_input = st.text_input("Pergunta:", value="")
 
 if user_input:
-    if "hi" in user_input.lower():
-        st.write("Bot: Hello! How can I help you today?")
-    elif "bye" in user_input.lower():
-        st.write("Bot: Goodbye! Have a nice day.")
+    if "what's your name?" in user_input.lower():
+        st.write("Bot: My name is Bot, nice to meet you!")
+    elif "what is the weather today?" in user_input.lower():
+        st.write("Bot: I am sorry, I am not capable of checking the weather. You can try searching for it online.")
+    elif "what time is it?" in user_input.lower():
+        st.write("Bot: The current time is {0}.".format(datetime.now().strftime("%H:%M")))
+    elif "what's your purpose?" in user_input.lower():
+        st.write("Bot: My purpose is to assist and answer questions to the best of my ability.")
+    elif "what do you do?" in user_input.lower():
+        st.write("Bot: I am a chatbot designed to answer questions and provide information.")
     else:
         st.write("Bot: Sorry, I don't understand. Can you try asking me something else?")
-
-
