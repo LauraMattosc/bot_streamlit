e89bf7d81d07cb7e8cb42694848a243c92d73b9d
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-02-08 18:12:54 -0300

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -5,22 +5,16 @@ st.title("Chatbot")
 
 user_input = st.text_input("Pergunta:", value="")
 
-questions = ["what's your name?",
-             "what is the weather today?",
-             "what time is it?",
-             "what's your purpose?",
-             "what do you do?"]
-
-answers = ["My name is Bot, nice to meet you!",
-           "I am sorry, I am not capable of checking the weather. You can try searching for it online.",
-           "The current time is {0}.".format(datetime.now().strftime("%H:%M")),
-           "My purpose is to assist and answer questions to the best of my ability.",
-           "I am a chatbot designed to answer questions and provide information."]
-
 if user_input:
-    for i, question in enumerate(questions):
-        if question in user_input.lower():
-            st.write("Bot: " + answers[i])
-            break
+    if "what's your name?" in user_input.lower():
+        st.write("Bot: My name is Bot, nice to meet you!")
+    elif "what is the weather today?" in user_input.lower():
+        st.write("Bot: I am sorry, I am not capable of checking the weather. You can try searching for it online.")
+    elif "what time is it?" in user_input.lower():
+        st.write("Bot: The current time is {0}.".format(datetime.datetime.now().strftime("%H:%M")))
+    elif "what's your purpose?" in user_input.lower():
+        st.write("Bot: My purpose is to assist and answer questions to the best of my ability.")
+    elif "what do you do?" in user_input.lower():
+        st.write("Bot: I am a chatbot designed to answer questions and provide information.")
     else:
         st.write("Bot: Sorry, I don't understand. Can you try asking me something else?")
