50b0688f8d513bff3a986cacbc88ea8b709f8258
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-03-19 16:24:38 -0300

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -4,6 +4,8 @@ import spacy
 
 nlp = spacy.load('pt_core_news_sm')
 
+st.title("Chatbot")
+
 def adicionar_pergunta_resposta(pergunta, resposta):
     global df
     nova_linha = pd.DataFrame({'Pergunta': [pergunta], 'Resposta': [resposta]})
@@ -19,14 +21,12 @@ pergunta = st.text_input("Faça uma pergunta:")
 # Criar DataFrame inicial com as perguntas de exemplo
 df = pd.DataFrame(exemplos)
 
-st.title("Chatbot")
-
 if pergunta:
     doc = nlp(pergunta)
     for token in doc:
         print(token.text, token.pos_, token.dep_)
     # Buscar a resposta no DataFrame
-    resposta = df[df['Pergunta'] == pergunta]['Resposta'].iloc[0]
+    resposta = df[df['Pergunta'] == pergunta]['Resposta'].iloc[0] if not df[df['Pergunta'] == pergunta].empty else "Desculpe, não sei a resposta para essa pergunta."
     # Adicionar a pergunta e a resposta ao DataFrame
     adicionar_pergunta_resposta(pergunta, resposta)
     st.write(resposta)
