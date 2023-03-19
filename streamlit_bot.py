93a3a4a4be8baf04f7b318429b21268a48e338ae
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-03-19 15:31:37 -0300

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -3,6 +3,9 @@ from datetime import datetime
 import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 
+import logging
+logging.basicConfig(level=logging.ERROR)
+
 # Inicializar o modelo BERT em português
 model = SentenceTransformer('neuralmind/bert-base-portuguese-cased')
 
