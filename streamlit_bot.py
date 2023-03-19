cf1a0bff98835d1705b34bfab79b221ba314b152
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-03-19 15:23:56 -0300

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from datetime import datetime
 import pandas as pd
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from sentence_transformers import SentenceTransformer, util
 
-# Inicializar o GPT-2
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-model = GPT2LMHeadModel.from_pretrained("gpt2")
+# Inicializar o modelo BERT em português
+model = SentenceTransformer('neuralmind/bert-base-portuguese-cased')
 
 st.title("Chatbot")
 
@@ -19,11 +18,23 @@ def adicionar_pergunta_resposta(pergunta, resposta):
     nova_linha = {'Pergunta': pergunta, 'Resposta': resposta}
     df = df.append(nova_linha, ignore_index=True)
 
+# Exemplo de perguntas e respostas
+exemplos = [
+    {'Pergunta': 'Qual é o seu nome?', 'Resposta': 'Meu nome é Chatbot.'},
+    {'Pergunta': 'Qual é o seu propósito?', 'Resposta': 'Meu propósito é responder às suas perguntas e ajudá-lo.'},
+    # Adicione mais exemplos aqui
+]
+
 user_input = st.text_input("Pergunta:", value="")
 if user_input:
-    entrada = tokenizer.encode(user_input, return_tensors="pt")
-    saida = model.generate(entrada, max_length=50, num_return_sequences=1, no_repeat_ngram_size=2, temperature=0.7)
-    resposta = tokenizer.decode(saida[0], skip_special_tokens=True)
+    # Calcular a similaridade entre a pergunta do usuário e as perguntas de exemplo
+    pergunta_embedding = model.encode(user_input, convert_to_tensor=True)
+    exemplos_embeddings = model.encode([ex['Pergunta'] for ex in exemplos], convert_to_tensor=True)
+    similaridades = util.pytorch_cos_sim(pergunta_embedding, exemplos_embeddings)
+
+    # Encontrar a pergunta de exemplo mais semelhante e usar sua resposta
+    resposta_indice = similaridades.argmax().item()
+    resposta = exemplos[resposta_indice]['Resposta']
 
     if resposta:
         adicionar_pergunta_resposta(user_input, resposta)
