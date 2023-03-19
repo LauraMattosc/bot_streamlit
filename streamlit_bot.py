7aafeac8e274b22b06609792a92d86316e7bb7db
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-03-19 15:54:07 -0300

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -1,16 +1,14 @@
 import streamlit as st
 import pandas as pd
-from sentence_transformers import SentenceTransformer, util
+import spacy
+
+# Inicializar o modelo do spaCy em português
+nlp = spacy.load("pt_core_news_sm")
 
 def adicionar_pergunta_resposta(pergunta, resposta):
     global df
     nova_linha = pd.DataFrame({'Pergunta': [pergunta], 'Resposta': [resposta]})
     df = pd.concat([df, nova_linha], ignore_index=True)
-
-# Inicializar o modelo BERT em português
-model = SentenceTransformer('neuralmind/bert-base-portuguese-cased')
-
-st.title("Chatbot")
 
 # Exemplo de perguntas e respostas
 exemplos = [
@@ -28,24 +26,8 @@ exemplos = [
     {'Pergunta': 'O que é fotossíntese?', 'Resposta': 'Fotossíntese é o processo pelo qual as plantas, algas e algumas bactérias convertem energia luminosa em energia química, produzindo glicose e oxigênio a partir de dióxido de carbono e água.'},
 ]
 
-
 # Criar DataFrame inicial com as perguntas de exemplo
 df = pd.DataFrame(exemplos)
 
-user_input = st.text_input("Pergunta:", value="")
-if user_input:
-    # Calcular a similaridade entre a pergunta do usuário e as perguntas de exemplo
-    pergunta_embedding = model.encode(user_input, convert_to_tensor=True)
-    exemplos_embeddings = model.encode([ex['Pergunta'] for ex in exemplos], convert_to_tensor=True)
-    similaridades = util.pytorch_cos_sim(pergunta_embedding, exemplos_embeddings)
+st.title("Chatbot")
 
-    # Encontrar a pergunta de exemplo mais semelhante e usar sua resposta
-    resposta_indice = similaridades.argmax().item()
-    resposta = exemplos[resposta_indice]['Resposta']
-
-    if resposta:
-        adicionar_pergunta_resposta(user_input, resposta)
-        st.write(resposta)
-
-# Exibir tabela com perguntas de exemplo
-st.table(df)
