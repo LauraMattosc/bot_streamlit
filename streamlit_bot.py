5f8de45a988b590dd60004c9a9143af905154d00
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-03-19 16:04:58 -0300

    Update streamlit_bot.py

--- streamlit_bot.py
+++ streamlit_bot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import spacy
 
-nlp = spacy.load('/path/to/pt_core_news_sm')
+nlp = spacy.load('pt_core_news_sm') 
 
 def adicionar_pergunta_resposta(pergunta, resposta):
     global df
